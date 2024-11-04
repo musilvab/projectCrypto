0b19345bea1e6ccb85d2025b367c99f60428f831
Lucas Gonçalves <lucasemanuel.gon@gmail.com>
2024-11-03 21:29:49 -0300

    Base html edit

--- projectCrypto-main/Project Crypto/Project-Crypto/src/main/java/com/crypto/Project/Crypto/controller/ViewController.java
+++ projectCrypto-main/Project Crypto/Project-Crypto/src/main/java/com/crypto/Project/Crypto/controller/ViewController.java
@@ -1,6 +1,7 @@
 package com.crypto.Project.Crypto.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -16,7 +17,7 @@ public class ViewController {
     }
 
     @GetMapping("/")
-    public String home() {
+    public String homePage() {
         return "home";
     }
 
